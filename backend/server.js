afde26140a742cf98afb48ef9e74f893658279f1
Zied Meddeb <ziedmeddeb123@outlook.com>
2023-11-22 18:24:17 +0100

    initialized project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require("cors");
+const connectmongodb = require('./config/config.js');
+const app = express();
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+const port = 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
+connectmongodb();
