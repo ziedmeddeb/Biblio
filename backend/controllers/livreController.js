9bc12058aa46c968232b67630224e33738b2de1d
Zied Meddeb <ziedmeddeb123@outlook.com>
2023-11-22 18:44:38 +0100

    added livre

--- backend/controllers/livreController.js
+++ backend/controllers/livreController.js
@@ -1,1 +1,30 @@
-console.log("Livre.js");
+const livreService=require('../services/livreService');
+const express=require('express');
+const livreController=express.Router();
+
+livreController.get('/',async(req,res)=>{
+    const livres=await livreService.getAllLivres();
+    res.json(livres);
+}
+);
+livreController.get('/:id',async(req,res)=>{
+    const livre=await livreService.getLivreById(req.params.id);
+    res.json(livre);
+}
+);
+livreController.post('/',async(req,res)=>{
+    const livre=await livreService.createLivre(req.body);
+    res.json(livre);
+}
+);
+livreController.put('/:id',async(req,res)=>{
+    const livre=await livreService.updateLivre(req.params.id,req.body);
+    res.json(livre);
+}
+);
+livreController.delete('/:id',async(req,res)=>{
+    const livre=await livreService.deleteLivre(req.params.id);
+    res.json(livre);
+}
+);
+module.exports=livreController;
